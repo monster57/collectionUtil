77c373ca912b8b0a9b384fcc971d5de21a32340c
monster57 <surajitb@thoughtworks.com>
2015-02-28 12:33:10 +0530

    tried with list with interface

--- spikes/spike.java
+++ spikes/spike.java
@@ -6,14 +6,16 @@ class car implements detail{
 	private String name;
 	private String model;
 	private int weight;
-	public car(String name , String model){
+	public car(String name , String model , int weight){
 		this.name = name;
 		this.model = model;
+		this.weight = weight;
 	}
 
 	public void Details(){
 		System.out.println("the name of the car is "+name);
 		System.out.println("the model of car is "+model);
+		System.out.println("the weight of the car is "+weight);
 	}
 
 }
@@ -28,20 +30,39 @@ class Airplane implements detail{
 		this.weight = weight;
 	} 
 	public void Details(){
-		System.out.println("the weight of the plane is "+weight);
+		System.out.println("the weight of the plane is "+name);
 		System.out.println("the name of the company "+name);
 		System.out.println("the name of the model is "+weight);
+	}
+}
+class companies implements detail{
+	private List<String> list;
+	public companies(List list){
+		this.list = list;
+	}
+
+	public void Details(){
+		System.out.println("the companies are ");
+		for(String ele : list){
+			System.out.println(ele);
+		}
 	}
 }
 
 class company{
 	public static void main(String[] args) {
-		detail honda =  new car("honda" , "B10");
-		detail AirIndia = new Airplane("AirIndia" , "A10" , 1124);
-		honda.Details();
-		AirIndia.Details();
+		List<String> all = new ArrayList<String>();
+		all.add("honda");
+		all.add("AirIndia");
+		detail[] allCompany = {
+			new car("honda" , "B10" , 490),
+			new Airplane("AirIndia"  , "A10" , 1124), 
+			new companies(all)
+		};
+		
+		for(detail ele:allCompany){
+			ele.Details();
+		}
 	}
 }
-
-
 
